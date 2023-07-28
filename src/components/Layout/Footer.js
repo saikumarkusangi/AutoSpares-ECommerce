9f55d2e6d58f4b811376bcd7afcee102b1463506
saikumarkusangi <saikumarkusangi007@gmail.com>
2023-07-28 16:44:06 +0530

    first deploy

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -2,7 +2,7 @@
 
 const Footer = ()=> {
     return(
-        <div className="sticky bottom-0 bg-[#326d74] py-4 text-center mt-5 mb-14">
+        <div className=" w-full h-max bg-[#326d74] py-4 text-center mt-5 mb-14">
             <p className="text-sm text-white">All Rights Reserved &copy; Sri Raghava Auto Spares</p>
         </div>
     )
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,16 +1,18 @@
 import {PiPackageThin} from "react-icons/pi";
 
-const ProductCard = (params)=> {
-    const {title,image,price} = params;
+const ProductCard = ({title,image,price})=> {
+   
+  return(
     <div className="w-[48%]  flex flex-col min-w-[48%]  relative">
-                 <span className=" bg-red-500 text-white text-sm px-2 rounded-lg absolute top-2 left-2">8% OFF</span>
-                     <img className="h-[50%] w-[100%] object-contain border-2 rounded-lg px-2 py-2 " src={image} alt="product"/>
-                     <h3 className="font-medium text-sm mt-1 line-clamp-2 h-[10%]">{title}</h3>
-                     <p className="text-gray-600 line-through">₹ {price+Math.floor(Math.random()*10)}</p>
-                     <p className=" text-xl font-medium">₹ {price}</p>
-                 <span className="text-green-600 font-medium text-sm flex gap-2 items-center"><PiPackageThin/>In Stock</span>
-                 <button className="bg-[#326d74] font-medium text-white  py-2 px-2 rounded-lg w-full mt-2" >Add to cart</button>
-             </div>
+    <span className=" bg-red-500 text-white text-sm px-2 rounded-lg absolute top-2 left-2">8% OFF</span>
+        <img className="h-[50%] w-[100%] object-contain border-2 rounded-lg px-2 py-2 " src={image} alt="product"/>
+        <h3 className="font-medium text-sm mt-1 line-clamp-2 h-[20%]">{title}</h3>
+        <p className="text-gray-600 line-through">₹ {price+Math.floor(Math.random()*10)}</p>
+        <p className=" text-xl font-medium">₹ {price}</p>
+    <span className="text-green-600 font-medium text-sm flex gap-2 items-center"><PiPackageThin/>In Stock</span>
+    <button className="bg-[#326d74] font-medium text-white  py-2 px-2 rounded-lg w-full mt-2" >Add to cart</button>
+</div>
+  )
 }
 
 export default ProductCard;
